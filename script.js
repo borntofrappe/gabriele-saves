84e21d278b77e0e4e8ca57805af77257d2462fb9
borntofrappe <borntofrappe@gmail.com>
2020-04-18 18:33:47 +0200

    update notes and script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// create a table of contents for every markdown file except README.md
 const fs = require('fs');
 const path = require('path');
 
@@ -6,16 +7,15 @@ const notes = files.filter(file => path.extname(file) === '.md' && file !== 'REA
 
 notes.forEach(note => {
   const content = fs.readFileSync(`./${note}`, { encoding: 'utf-8' });
-  if(!/# Index/.test(content)) {
     const regExp = /(#{2,6}) (.+)/g;
 
-    let match;
-    const matches = [];
-    while((match = regExp.exec(content)) !== null) {
-      const [, spaces, title] = match;
-      matches.push([spaces, title]);
-    }
-    const tableOfContents = [...matches].map(([spaces, title]) => `${'  '.repeat(spaces.length - 2)}- [${title}](#${title.replace(' ', '-')})`).join('\n\n');
-    fs.writeFileSync(note, `# Index\n\n${tableOfContents}\n\n${content}`);
+  let match;
+  const matches = [];
+  while((match = regExp.exec(content)) !== null) {
+    const [, spaces, title] = match;
+    const {index} = match;
+    matches.push([spaces, title, index]);
   }
+  const tableOfContents = matches.map(([spaces, title]) => `${'  '.repeat(spaces.length - 2)}- [${title}](#${title.replace(' ', '-')})`).join('\n\n');
+  fs.writeFileSync(note, `# Index\n\n${tableOfContents}\n\n${content.slice(matches[0][2])}`);
 });

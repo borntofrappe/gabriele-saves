46917b2b8f6ae659d393a3affb9aab8832c72255
borntofrappe <borntofrappe@gmail.com>
2020-04-19 14:13:30 +0200

    create saves folder

--- prepend-toc.js
+++ prepend-toc.js
@@ -1,12 +1,12 @@
-// create a table of contents for every markdown file except README.md
+// create a table of contents for every markdown file in the ./saves folder
 const fs = require('fs');
 const path = require('path');
 
-const files = fs.readdirSync('.', 'utf-8');
-const notes = files.filter(file => path.extname(file) === '.md' && file !== 'README.md');
+const files = fs.readdirSync('./saves', 'utf-8');
+const saves = files.filter(file => path.extname(file) === '.md');
 
-notes.forEach(note => {
-  const content = fs.readFileSync(`./${note}`, { encoding: 'utf-8' });
+saves.forEach(save => {
+  const content = fs.readFileSync(`./saves/${save}`, { encoding: 'utf-8' });
     const regExp = /(#{2,6}) (.+)/g;
 
   let match;
@@ -17,5 +17,5 @@ notes.forEach(note => {
     matches.push([spaces, title, index]);
   }
   const tableOfContents = matches.map(([spaces, title]) => `${'  '.repeat(spaces.length - 2)}- [${title}](#${title.replace(' ', '-')})`).join('\n\n');
-  fs.writeFileSync(note, `# Index\n\n${tableOfContents}\n\n${content.slice(matches[0][2])}`);
+  fs.writeFileSync(`./saves/${save}`, `# Table of Contents\n\n${tableOfContents}\n\n${content.slice(matches[0][2])}`);
 });
